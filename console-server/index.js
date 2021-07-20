d87909b25996342b0afde5b0201469890bfd2d63
Maximiliano Arias <maximiliano.arias@fidtech.net>
2021-07-20 17:23:58 -0300

    feat(cloudwatch): Added cloudwatch support using wisnton

--- console-server/index.js
+++ console-server/index.js
@@ -1,6 +1,31 @@
 const app = require('http').createServer();
 const { version } = require('../package.json');
 
+// Winston logger
+const winston  = require('winston');
+const WinstonCloudWatch = require('winston-cloudwatch');
+
+const logger = winston.createLogger({
+	transports: [ new winston.transports.Console()]
+});
+
+const AWS = require('aws-sdk');
+
+AWS.config.update({
+	region: 'sa-east-1'
+});
+
+const dateObj = new Date();
+const month = dateObj.getUTCMonth() + 1; //months from 1-12
+const day = dateObj.getUTCDate();
+const year = dateObj.getUTCFullYear();
+
+winston.add(new WinstonCloudWatch({
+	logGroupName: 'photofied-client-logs',
+	logStreamName: `logs_${year}_${month}_${day}`
+}));
+// End windston logger
+
 require('custom-env').env(process.env.NODE_ENV || 'development');
 
 if (process.env.USE_PORT) process.env.PORT = process.env.USE_PORT;
@@ -48,8 +73,31 @@ io.on('connection', function (socket) {
 		if (data.loopback) {
 			io.to(data.channel).emit('toConsoleRe', data);
 		} else {
+			let logLevel = data.level;
+			if(data.level === 'log')
+				logLevel = 'info';
+			winston.log(logLevel, preprocess(data));
 			socket.broadcast.to(data.channel).emit('toConsoleRe', data);
 		}
 		if (cb !== undefined) cb('success');
 	});
 });
+
+const preprocess = function(data){
+	if(data && data.args){
+		data.args = data.args.map(d=>{
+			let d1 = d.replace("[color=red]","");
+			d1 = d1.replace("[/color]","");
+			if (/^[\],:{}\s]*$/.test(d1.replace(/\\["\\\/bfnrtu]/g, '@').
+			replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']').
+			replace(/(?:^|:|,)(?:\s*\[)+/g, ''))) {
+				d1 = JSON.parse(d1);
+			}
+			return d1;
+		});
+		return JSON.stringify(data);
+	}else{
+		return "";
+	}
+
+};
